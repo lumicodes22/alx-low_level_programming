4821c84f515274eb13b7176a9a900b23aec4ba2d
lumicodes22 <lumicodes1@gmail.com>
2023-08-10 19:05:03 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is a program that prints with putts function
+ *
+ * Return: 0 (Goes sucessfully)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

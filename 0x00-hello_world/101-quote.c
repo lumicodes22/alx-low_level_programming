83a8a50558dbad74d318f21fe30c61b4f72b675c
lumicodes22 <lumicodes1@gmail.com>
2023-08-11 03:23:26 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unstdio.h>
+/**
+ * main - A c program that prints a line in the standard c error
+ *
+ * Return 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}

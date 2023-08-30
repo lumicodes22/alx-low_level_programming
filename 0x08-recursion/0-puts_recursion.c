87a06b43642e4ef0b739adb11301d682ffe1756f
lumicodes22 <lumicodes1@gmail.com>
2023-08-30 10:34:47 -0700

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  * _puts_recursion - fills memory with a constant byte.
+ *   * @s: first bytes of the memory
+ *    * Return: -
+ *     */
+void _puts_recursion(char *s)
+{
+		if (*s == '\0')
+				{
+							_putchar('\n');
+									return;
+										}
+			_putchar(*s);
+				s++;
+					_puts_recursion(s);
+}

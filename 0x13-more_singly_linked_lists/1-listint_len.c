62ce44417e467722e80e223e328b71e18177ee32
lumicodes22 <lumicodes1@gmail.com>
2023-09-25 13:49:33 -0700

    debugged

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements pf a list_t list
- * 
- * @h: pointer to the head of the list.
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
  *
- * Return: numbers of nodes.
+ * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t nofnodes = 0;
+	size_t num = 0;
 
-	while (h != NULL)
+	while (h)
 	{
+		num++;
 		h = h->next;
-		nofnodes++;
 	}
-	return (nofnodes);
+
+	return (num);
 }

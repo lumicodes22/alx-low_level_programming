a5fcdfabcc6a0d88fb6ed9f4f04e795866848247
lumicodes22 <lumicodes1@gmail.com>
2023-10-30 06:17:39 -0700

    Created a function that reads a text file and prints it to the POSIX standard output.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - Converts a binary string to an unsigned integer.
+ * @b: A pointer to the binary string to be converted.
+ *
+ * Return: The converted decimal number or 0 if there is
+ * an unconvertable character.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int total, power;
+	int len;
+
+	if (b == NULL)
+		return (0);
+
+	for (len = 0; b[len]; len++)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+	}
+
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	{
+		if (b[len] == '1')
+			total += power;
+	}
+
+	return (total);
+}
